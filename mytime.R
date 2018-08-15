bc8e725bbf622d1b3e9578dd2e1d844bac0896c7
Jonathan Dayton <jonathandayton23@gmail.com>
2018-08-15 16:54:25 -0600

    Use pacman to load & install packages

--- mytime.R
+++ mytime.R
@@ -1,13 +1,8 @@
 #!/usr/bin/env Rscript
 
-library(googlesheets4)
-options(warn = -1)
-suppressMessages(
-  library(tidyverse, quietly = TRUE)
-)
-suppressMessages(
-  library(lubridate)
-)
+if (!require("pacman", quietly = TRUE)) install.packages("pacman")
+p_load_gh("hadley/tidyverse", "tidyverse/googlesheets4")
+p_load("lubridate")
 
 sheet_url <- "https://docs.google.com/spreadsheets/d/11oDqgcAzuKgF1550oCSY7fP_5iyH5nrTuZtAwK23-Uk/edit?usp=sharing"
 
